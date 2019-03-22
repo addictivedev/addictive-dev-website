f74628feb7bdf23c2900bb28e0142d032a3aeaf4
bugman <onlyforwin@gmail.com>
2019-03-22 11:28:52 +0100

    NEW: add copy for index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,42 +30,60 @@ class HomeIndex extends React.Component {
             <article style={{ backgroundImage: `url(${pic01})` }}>
               <header className="major">
                 <h3>Web applications</h3>
-                <p>Ipsum dolor sit amet</p>
+                <p>
+                  We provide both frontend and backend development services.
+                  <br />
+                  Our stack includes Elixir, Phoenix, Ruby on Rails, Javascript, Ember.js, React,
+                  React Native.
+                </p>
               </header>
               <Link to="/services" className="link primary" />
             </article>
             <article style={{ backgroundImage: `url(${pic02})` }}>
               <header className="major">
                 <h3>Mobile applications</h3>
-                <p>feugiat amet tempus</p>
+                <p>
+                  We provide cross platform mobile apps using React Native, with
+                  a touch of native development.
+                </p>
               </header>
               <Link to="/services" className="link primary" />
             </article>
             <article style={{ backgroundImage: `url(${pic03})` }}>
               <header className="major">
-                <h3>Backend services</h3>
-                <p>Lorem etiam nullam</p>
+                <h3>UI/UX Design</h3>
+                <p>
+                  We offer prototyping, wireframing, branding, UI and UX design
+                </p>
               </header>
               <Link to="/services" className="link primary" />
             </article>
             <article style={{ backgroundImage: `url(${pic04})` }}>
               <header className="major">
-                <h3>Third Party integrations</h3>
-                <p>Nisl sed aliquam</p>
+                <h3>Blockchain and Dapps development</h3>
+                <p>
+                  We can create and deploy full Dapps and contracts on the Ethereum blockchain.
+                </p>
               </header>
               <Link to="/services" className="link primary" />
             </article>
             <article style={{ backgroundImage: `url(${pic05})` }}>
               <header className="major">
                 <h3>Adtech specialists</h3>
-                <p>Ipsum dolor sit amet</p>
+                <p>
+                  We have an extensive experience working with Facebook Ads API and
+                  we can create tailored solutions for your business.
+                </p>
               </header>
               <Link to="/services" className="link primary" />
             </article>
             <article style={{ backgroundImage: `url(${pic06})` }}>
               <header className="major">
                 <h3>Consultancy</h3>
-                <p>Feugiat amet tempus</p>
+                <p>
+                  Do you have a challenging problem that need to be solved?
+                  Hire us!
+                </p>
               </header>
               <Link to="/services" className="link primary" />
             </article>
@@ -73,16 +91,13 @@ class HomeIndex extends React.Component {
           <section id="two">
             <div className="inner">
               <header className="major">
-                <h2>Massa libero</h2>
+                <h2>We create apps, big and small</h2>
               </header>
               <p>
-                Nullam et orci eu lorem consequat tincidunt vivamus et sagittis
-                libero. Mauris aliquet magna magna sed nunc rhoncus pharetra.
-                Pellentesque condimentum sem. In efficitur ligula tate urna.
-                Maecenas laoreet massa vel lacinia pellentesque lorem ipsum
-                dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et
-                sagittis libero. Mauris aliquet magna magna sed nunc rhoncus
-                amet pharetra et feugiat tempus.
+                We offer full-stack mobile and web development services
+                together with requirements analysis, quality assurance, and project
+                management. We have all the expertise you need to produce a fully
+                fledged, stable, and scalable product.
               </p>
               <ul className="actions">
                 <li>
