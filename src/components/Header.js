f87d1de5d7bed0a8c948656d2e823fac9edb8d6e
Mirco Cotugno <mirco@pitchtarget.com>
2019-05-16 12:36:30 +0200

    new logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,12 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { Link } from 'gatsby-plugin-i18next';
 import { translate } from 'react-i18next';
+import logo from '../assets/images/logo-addictive.svg';
 
 const Header = ({children, data, t, onToggleMenu}) => (
   <header id="header" className="alt">
     <Link to="/" className="logo">
-      <strong>Addictive</strong>
+      <img className="logo--image" src={logo} alt="Addictive"/>
     </Link>
     <nav>
       <button className="menu-link" onClick={onToggleMenu}>
