8dd3bab43a22d6f61b4d52c5517686c2e881a7aa
bugman <onlyforwin@gmail.com>
2019-06-19 16:54:28 +0200

    FIX: convert all intl.formattedMesssage to FormattedMessage component

--- src/components/redirect.js
+++ src/components/redirect.js
@@ -1,9 +1,9 @@
 import React from "react"
-import { injectIntl } from "gatsby-plugin-intl"
+import { injectIntl, FormattedMessage } from "gatsby-plugin-intl"
 import SEO from "./SEO"
 
 const Redirect = ({ intl }) => {
-  return <SEO title={`${intl.formattedMessage({ id: "title" })}`} />
+  return <SEO title={<FormattedMessage id="title" />} />
 }
 
 export default injectIntl(Redirect)
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
-import { injectIntl } from "gatsby-plugin-intl"
+import { injectIntl, FormattedMessage } from "gatsby-plugin-intl"
 
 const NotFoundPage = ({intl}) => (
   <Layout>
     <div id="main" className="alt">
       <section id="one">
         <div className="inner">
-          <h1>{intl.formattedMessage({id: 'pages.404.title'})}</h1>
-          <p>{intl.formattedMessage({id: 'pages.404.error'})}</p>
+          <h1><FormattedMessage id='pages.404.title' /></h1>
+          <p><FormattedMessage id='pages.404.error' /></p>
         </div>
       </section>
     </div>
